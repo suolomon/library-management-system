0886f89eb7749e392cd333881a70139b66c17b41
derrimugisha <derrimugisha@gmail.com>
2020-03-20 01:17:11 +0300

    changes done with flake8

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import client,book_store
+from .models import Client, BookStore
 
-admin.site.register(client)
-admin.site.register(book_store)
+admin.site.register(Client)
+admin.site.register(BookStore)
